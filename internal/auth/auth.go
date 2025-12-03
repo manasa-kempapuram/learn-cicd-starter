eb71795322e6ff04fc301cb58059b27a93c92d4a
Manasa Shivaram <manasashivaram@mac.speedport.ip>
2025-12-03 21:31:11 +0100

    Temporarily break GetAPIKey to test CI failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,25 +3,11 @@ package auth
 import (
 	"errors"
 	"net/http"
-	"strings"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
-// GetAPIKey -
-
-	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
-	}
-
-	return splitAuth[1], nil
-}
-
-
+// GetAPIKey temporarily broken to test CI
 func GetAPIKey(headers http.Header) (string, error) {
-	// Broken version to test CI failure
 	return "BROKEN_KEY", nil
 }
-
-
