d960c7cc9e56fb80c83a331c05916cab687cbac6
Manasa Shivaram <manasashivaram@mac.speedport.ip>
2025-12-03 21:36:52 +0100

    Temporarily break GetAPIKey to test CI failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -5,9 +5,10 @@ import (
 	"net/http"
 )
 
+// Error for missing header
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
-// GetAPIKey temporarily broken to test CI
+// Temporarily broken GetAPIKey for CI failure
 func GetAPIKey(headers http.Header) (string, error) {
 	return "BROKEN_KEY", nil
 }
